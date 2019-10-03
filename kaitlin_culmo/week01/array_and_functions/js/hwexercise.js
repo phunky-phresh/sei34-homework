f1682224d10d6cfa787f4f20a9c03950db9d03da
Kaitlin Culmo <culmo.kaitlin@gmail.com>
2019-10-04 09:14:50 +1000

    array_functions_objects_hw

---
+++ kaitlin_culmo/week01/array_and_functions/js/hwexercise.js
@@ -0,0 +1,59 @@
+// Define a function maxOfTwoNumbers that takes two numbers as arguments and returns
+// the largest of them. Use the if-then-else construct available in Javascript. You'll
+// have to remember your pre-work, or do some googling to figure this out.
+
+
+const maxOfTwoNumbers = function (2, i)
+if (2 > i) {
+ return 2;
+} else if (2 <= i) {
+  return i;
+}
+
+console.log( i >= 3 );
+
+// Define a function maxOfThree that takes three numbers as arguments and returns
+// the largest of them.
+
+
+
+
+
+// Write a function that takes a character (i.e. a string of length 1) and returns
+// true if it is a vowel, false otherwise.
+
+const vowels = ['a', 'e', 'i', 'o', 'u']
+var letter = 'a';
+  if (letter = vowels) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+
+
+// Define a function sumArray and a function multiplyArray that sums and
+// multiplies (respectively) all the numbers in an array of numbers. For example,
+// sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+
+let sumArray = [1, 2, 3, 4, 5];
+  const add = (a, b) => (a + b);
+  const sum = numbers.reduce(add);
+
+console.log(sum);
+
+
+// const multiplyArray = [1, 2, 3, 4]
+
+
+
+//*define function of reverse string
+
+function reverseString(string) {
+  let newString = "";
+  for (var i = string.length-1; i >= 0; 1--) {
+    newString += string[i];
+  }
+  return neString;
+}
+
+console.log(reverseString('wordword'));
