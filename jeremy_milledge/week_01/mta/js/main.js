be0e29fe95b3b61fadfbffaa09ba11f06acc05c4
Jeremy Milledge <jez.milledge@gmail.com>
2019-10-05 16:10:55 +1000

    added basic webpage interactivity

--- jeremy_milledge/week_01/mta/js/main.js
+++ jeremy_milledge/week_01/mta/js/main.js
@@ -1,11 +1,33 @@
 // MTA Week 01
 
-const nycSubway = {
+
+// INPUTS AND STORAGE //////////////////////////////////////////////////////////
+
+const nycSubway = { //inputs for subset of MTA lines
   N: ['8th', 'Union Square', '23rd', '28th', '34th', 'Times Square'],
   L: ['8th', '6th', 'Union Square', '3rd', '1st'],
   6: ['Astor Place', 'Union Square', '23rd', '28th', '33rd', 'Grand Central']
 };
 
+const tripDB = []; // collection of all Trip objects
+
+// Trip OBJECT CONSTRUCTION FUNCTIONS //////////////////////////////////////////
+
+const Trip = function(startL, startS, endL, endS) {
+  /*creates Trip object to log from with planTrip */
+
+  // TODO:  parse start and end lines/station to ensure they make sense
+  //        don't just blindly add them to the object if they dont
+  this.legs = findAllStops(startL, startS, endL, endS);
+  this.start = startS;
+  this.startLine = startL;
+  this.end = endS;
+  this.endLine = endL;
+  this.transfers = findTransfers(this.legs);
+  this.numLegs = this.legs.length;
+
+  tripDB.push(this); //add to trip database
+}
 
 const pushStops = function(start, end, line) {
   /* push stations on specified line to returned array */
@@ -44,67 +66,18 @@ const findAllStops = function(startL, startS, endL, endS) {
   }
   if (startL == endL) { //only one train to take
     firstLeg = singleLineStops(startL, startS, endS);
-    stops.push(firstLeg);
   } else { //need to take two trains
     firstLeg = singleLineStops(startL, startS); //only take as far as U.S
-    stops.push(firstLeg);
     //new line is our 'finish line', new start is U.S, stop is finish dest.
-    const secondLeg = singleLineStops(endL, 'Union Square', endS);
-    stops.push(secondLeg);
+    stops.push(singleLineStops(endL, 'Union Square', endS));
   }
-  return stops; //array of shape [[first leg stops], [second leg stops]]
+  stops.unshift(firstLeg);
+  return stops; //array of shape [[first leg stops], [second leg stops]...]
 };
-
-const planTrip = function(startL, startS, endL, endS) {
-  /* function to return string for loging to console*/
-
-  let msg = '==============================================\n';
-  msg += `Line ${startL}, ${startS} --> Line ${endL}, ${endS}\n`
-  msg += '==============================================\n\n';
-  //array of shape [[first leg stops], [second leg stops]]
-  const allStops = findAllStops(startL, startS, endL, endS);
-  const numLegs = allStops.length;
-
-  const lines = [startL, endL];
-
-  for (let i = 0; i < numLegs; i ++) {
-    const thisLeg = allStops[i];
-    const legLen = thisLeg.length;
-    const firstInLeg = thisLeg.shift(); //removes doubled-up entry of transfer station
-    const lastInLeg = thisLeg.pop(); // in order to
-    msg = !i ? msg + `You get on at ${firstInLeg}.\n\n`: msg;
-
-    msg += `Leg #${i + 1}/${numLegs} on the ${lines[i]} line:\n`;
-    for (let j = 0; j < legLen - 2; j++) {
-      msg += ` > ${thisLeg[j]}\n`;
-    }
-    if (numLegs > 1 && i !== numLegs - 1) {
-      msg += `\nChange at ${lastInLeg}.\n\n`;
-    } else {
-      msg += `\nYou arrive at ${lastInLeg}.`
-    }
-  }
-  return msg;
-};
-
-const Trip = function(startL, startS, endL, endS) {
-  /*creates Trip object to log from with planTrip */
-
-  // TODO:  parse start and end lines/station to ensure they make sense
-  //        don't just blindly add them to the object if they dont
-  this.legs = findAllStops(startL, startS, endL, endS);
-  this.start = startS;
-  this.startLine = startL;
-  this.end = endS;
-  this.endLine = endL;
-  this.transfers = findTransfers(this.legs);
-  this.numLegs = this.legs.length;
-  //this.log = logTrip(this)
-
-  tripDB.push(this); //add to trip database
-}
 
 const findTransfers = function(arr) {
+  /* loop  through legs and retrieve last stop for all but last leg */
+
   tfers =[]
   for (let i = 0; i < arr.length - 1; i++) {
     tfers.push(arr[i][arr[i].length-1]);
@@ -112,39 +85,13 @@ const findTransfers = function(arr) {
   return tfers;
 }
 
-let logTrip = function(trip) {
+//LOGGING FUNCTIONS (FOR DISPLAY IN CONSOLE ONLY) //////////////////////////////
+
+const logTrip = function(Trip) {
   /* returns string for logging to console */
   /* input must be a Trip instance */
 
-  let msg = logHeader(trip);
-
-  for (let i = 0; i < trip.numLegs; i ++) { //loop to allow for later legs to be added
-    let leg = trip.legs[i]; //copies so that we don't mess with Trip object
-    const legFirst = leg[0]; //leg.shift() affecting object itself?????
-    const legLast = leg[leg.length-1]; // leg.pop() ^ here as well
-    msg += logGetOn(legFirst, i);
-    msg += logLegs(leg)
-    msg += logTransferOrFinal(trip.numLegs, legLast, trip.endLine, i);
-  }
-  return console.log(msg);
-};
-
-const logGetOn = function(sS, idx) {
-  return (
-    !idx ? `~ Get on at ${sS}.\n\\\n \\\n`: ""
-  );
-};
-
-const logTransferOrFinal = function(num, last, lastLine, idx) {
-  return (num > 1 && idx !== num - 1) ? ` /\n/\n~ Change at ${last} to Line ${lastLine}.\n\\\n \\\n` : ` /\n/\n~ Arrival at ${last}.\n `;
-};
-
-const logLegs = function(legArr) {
-  let leg = '';
-  for (let i = 1; i < legArr.length - 1; i++) {
-    leg += ` |\n + ${legArr[i]}\n |\n`;
-  }
-  return leg;
+  return console.log(logHeader(Trip) + logBody(Trip));
 };
 
 const logHeader = function(t) {
@@ -155,10 +102,42 @@ Line ${t.startLine}, ${t.start} --> Line ${t.endLine}, ${t.end}\n\
   );
 };
 
-let tripDB = []; // collection of all Trip objects
+const logBody = function(trip) {
+  bodyMsg = '';
+  for (let i = 0; i < trip.numLegs; i ++) { //loop to allow for later legs to be added
+    let leg = trip.legs[i]; //copies so that we don't mess with Trip object
+    bodyMsg += logGetOn(leg[0], i);
+    bodyMsg += logLegs(leg)
+    bodyMsg += logTransferOrFinal(trip.numLegs, leg[leg.length-1], trip.endLine, i);
+  }
+  return bodyMsg
+};
 
+const logGetOn = function(sS, idx) {
+  return (
+    !idx ? `~ Get on at ${sS}.\n\\\n \\\n`: ""
+  );
+};
 
-//TEST INPUTS ==================================
+const logLegs = function(legArr) {
+  let leg = '';
+  for (let i = 1; i < legArr.length - 1; i++) {
+    leg += ` |\n + ${legArr[i]}\n |\n`;
+  }
+  return leg;
+};
+
+const logTransferOrFinal = function(numLegs, last, lastLine, idx) {
+  changeMsg = ' /\n/\n~ ';
+  if (numLegs > 1 && idx !== numLegs - 1) { //more than 1 leg and not last leg
+    changeMsg += `Change at ${last} to Line ${lastLine}.\n\\\n \\\n`;
+  } else {
+    changeMsg += `Arrival at ${last}.\n `
+  }
+  return  changeMsg;
+};
+
+// TEST INPUTS /////////////////////////////////////////////////////////////////
 const testTrips = [
   ['N', 'Times Square', 'N', '8th'],
   ['L', '1st', '6', 'Grand Central'],
@@ -169,13 +148,27 @@ const testTrips = [
 
 for (let k = 0; k < testTrips.length; k++) {
   let t = testTrips[k];
-  new Trip(t[0], t[1], t[2], t[3]);
-  logTrip(tripDB[k]);
+  new Trip(t[0], t[1], t[2], t[3]); //constructor pushes to DB automatically
+  logTrip(tripDB[k]); //log from DB
 }
 
 let eg = tripDB[2];
 
-// for (let k = 0; k < testTrips.length; k++) {
-//   let t = testTrips[k];
-//   console.log(planTrip(t[0], t[1], t[2], t[3]));
-// }
+
+// WEBPAGE INTERACTIVITY ///////////////////////////////////////////////////////
+//let lines = [];
+//let stations = [];
+
+
+const buildFromClick = function() {
+
+  let sL = document.getElementById('startL').value;
+  let sS = document.getElementById('startS').value;
+  let eL = document.getElementById('endL').value;
+  let eS = document.getElementById('endS').value;
+  let t = new Trip(sL, sS, eL, eS)
+  return logTrip(t);
+};
+
+let btn = document.getElementById('generate');
+let btnListener = btn.addEventListener('click', buildFromClick);
