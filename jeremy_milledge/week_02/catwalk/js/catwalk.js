5b9aee8f010c8794d5a7df37624c6e789b0f9856
Jeremy Milledge <jez.milledge@gmail.com>
2019-10-10 22:46:49 +1100

    gamified

--- jeremy_milledge/week_02/catwalk/js/catwalk.js
+++ jeremy_milledge/week_02/catwalk/js/catwalk.js
@@ -1,27 +1,38 @@
 
 const btn = document.getElementById('add-cat');
+const again = document.getElementById('again');
+const score = document.getElementById('score');
+const gameoverPanel = document.getElementById('gameover');
+const notice = document.getElementById('notice');
+//add meow sounds
 let meows = [];
 for (let i = 1; i <= 7; i++) {
-  meows.push(new Audio('sfx/meow'+i+'.wav'))
+  meows.push(new Audio(`sfx/meow${i}.wav`))
 }
-let count = 0;
+//add cat gifs
+let cats = [];
+for (let i = 1; i <= 8; i++) {
+  cats.push(`gif/cat${i}.gif`);
+}
+
 let y = window.innerHeight - 296;
 let x = window.innerWidth - 296;
-let cattery = []
+
 
 const newCat = function() {
   let c = document.createElement('img');
-  c.setAttribute('src', 'http://www.anniemation.com/clip_art/images/cat-walk.gif');
+
+  c.setAttribute('src', cats[rand(8)]);
   c.style.top = rand(y) + 'px';
   c.style.left = rand(x) + 'px';
-  c.style.width = rand(400,20) + 'px';
-  c.style.webkitTransform = `rotate(${rand(360)}deg)`;
-  c.id = count;
-  count++
-  document.body.appendChild(c);
+  c.style.width = rand(x/1.5, 40) + 'px';
+  c.style.webkitTransform += `rotate(${rand(360)}deg)`;
+  score.innerText = parseInt(score.innerText) + 1;
+  c.addEventListener('click', gameOver);
+  document.getElementById('cats').appendChild(c);
 
   //set interval
-  cattery.push(window.setInterval(bounceFlip,100, c, rand(80,10), rand(80,10)));
+  window.setInterval(bounceFlip,100, c, rand(80,10), rand(80,10));
 
   //bg adjustment
   let randRGB = `rgb(${rand(255)},${rand(255)},${rand(255)})`
@@ -32,9 +43,29 @@ const newCat = function() {
   btn.style.top = rand(y) + 'px'
 
   //sound
-  let meow = meows[rand(8,1)];
+  let meow = meows[rand(7)];
   meow.play();
   meow.currentTime=0;
+
+
+}
+
+function gameOver() {
+  let allCats = document.getElementById('cats');
+  btn.style.display = 'none';
+  score.style.display = 'none';
+  while (allCats.firstChild) {
+    allCats.removeChild(allCats.firstChild);
+  }
+  notice.innerText = score.innerText;
+  gameoverPanel.style.display = 'flex';
+}
+
+function startAgain() {
+  btn.style.display =  'block';
+  score.style.display = 'block';
+  gameoverPanel.style.display = 'none'
+  score.innerText = '0';
 }
 
 function rand(max, min=0) {
@@ -42,9 +73,10 @@ function rand(max, min=0) {
   return  n < min ? n + min : n;
 }
 
-function getMovement(obj, dm, max, dir, cls) {
+function getMovement(obj, dm, max, dir) {
   let m0 = parseInt(obj.style[dir]);
   let m1 = m0;
+  let cls = dir === 'left' ? 'xback' : 'yback';
 
   if (m0 > max) {
     obj.classList.add(cls);
@@ -62,9 +94,9 @@ function getMovement(obj, dm, max, dir, cls) {
 }
 
 const bounceFlip = function(cat, dx, dy) {
-  cat.style.left = getMovement(cat, dx, x, 'left', 'xback') + 'px';
-  cat.style.top = getMovement(cat, dy, y, 'top', 'yback') + 'px';
+  cat.style.left = getMovement(cat, dx, x, 'left') + 'px';
+  cat.style.top = getMovement(cat, dy, y, 'top') + 'px';
 }
 
-
+again.addEventListener('click',startAgain);
 btn.addEventListener('click', newCat)
