bb03381853c818e0c83330c169df216a5846eedb
Amanda Chau <amandachau36@gmail.com>
2019-10-20 16:19:06 +1100

    updated text soup

---
+++ warmup/week04/day01_text_soup/js/main.js
@@ -0,0 +1,3 @@
+// - Use jQuery to get the contents of the div as a string, then split up the string into an array of words.
+// - Re-use the `getRandomElementFromArray()` function we wrote at the end of week 1 to pick a random word from the array.
+// - Use jQuery to 1) create a new DIV with that word as its contents, 2) set the DIV to appear at a random position on the page , and 3) append it to the DOM.
