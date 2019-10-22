0510215c68c019612be3f41b2ead780261773222
ncooney94 <nick.cooney@gmail.com>
2019-10-23 08:59:25 +1100

    trying to DRY

--- nicholas_cooney/week04/MTA-lab-ruby/MTA.rb
+++ nicholas_cooney/week04/MTA-lab-ruby/MTA.rb
@@ -1,5 +1,3 @@
-require 'pry'
-# TODO make the train lines a visible menu on terminal
 
   $mta = {
     "lineN" => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"],
@@ -21,14 +19,8 @@ end
 show_menu
 menu_choice = gets.chomp.downcase
 
-# start = trainLines[:lineN].index("34th")
-# stop = trainLines[:lineN].index("8th")
-#
-# journey = trainLines[:lineN][start..stop].join(', ')
-#
-# p "these are your stops #{journey}"
 
-# create a method that takes parameters for start and finish
+
 def trip(first, on, last, off)
   start = $mta[first].index(on)
   union1 = $mta[first].index("Union Square")
@@ -38,52 +30,69 @@ def trip(first, on, last, off)
   elsif start < union1
     firstLeg = $mta[first][start..union1].join(', ')
   end
-  # binding.pry
 
   finish = $mta[last].index(off)
   union2 = $mta[last].index("Union Square")
-  # if finish > union2
   reverse = $mta[last][finish..union2].reverse
   reverse.shift
   lastLeg = reverse.join(', ')
-  p "#{firstLeg + ', ' + lastLeg}"
+  journey = n + reverse
+  length = journey.length
+  p "You must travel through the following stops on the #{first}: #{firstLeg}."
+  p "Change at Union Square."
+  p "Your journey continues through the following stops: #{lastLeg}."
+  p "#{length} stops in total."
+
   show_menu
   menu_choice = gets.chomp.downcase
 end
-
-until menu_choice == "q"
-  puts "you chose: #{menu_choice}"
-  case menu_choice
-when "s"
+#
+def line
   puts "Start line: "
-  line = gets.chomp
+  $line = gets.chomp
+end
+def start
   puts "Start station: "
   on = gets.chomp
+  result = $mta[$line].index(on)
+end
+def stop
   puts "Finish station: "
   off = gets.chomp
-  start = $mta[line].index(on)
-  stop = $mta[line].index(off)
-  journey = $mta[line][start..stop].join(', ')
-  p "These are your stops #{journey}"
+  result = $mta[$line].index(off)
+end
+#
+  until menu_choice == "q"
+    puts "you chose: #{menu_choice}"
+    case menu_choice
+    when "s"
+      # puts "Start line: "
+      # line = gets.chomp
+      # puts "Start station: "
+      # on = gets.chomp
+      # puts "Finish station: "
+      # off = gets.chomp
+      # start = $mta [line].index(on)
+      # stop = $mta[line].index(off)
+      journey = $mta[line()][start()..stop()].join(', ')
+      p "These are your stops #{journey}"
+      show_menu
+      menu_choice = gets.chomp.downcase
+      require 'pry'
+    when "m"
+      puts "Start line: "
+      first = gets.chomp
+      puts "Start station: "
+      on = gets.chomp
+      puts "Finish line: "
+      last = gets.chomp
+      puts "Finish station: "
+      off = gets.chomp
+
+      p "#{trip(first, on, last, off)}"
+  else
+    puts "Invalid selection, stupid."
   show_menu
   menu_choice = gets.chomp.downcase
-when "m"
-  puts "Start line: "
-  first = gets.chomp
-  puts "Start station: "
-  on = gets.chomp
-  puts "Finish line: "
-  last = gets.chomp
-  puts "Finish station: "
-  off = gets.chomp
-  p "#{trip(first, on, last, off)}"
-
+  end
 end
-end
-# trip(:lineN, "8th", :line6, "33rd")
-# trip(:lineL, "3rd", :lineN, "Times Square")
-# trip(:line6, "Grand Central", :lineL, "8th")
-# binding.pry
-# # log the index of start and finish
-#change at union Square
-# print stops
