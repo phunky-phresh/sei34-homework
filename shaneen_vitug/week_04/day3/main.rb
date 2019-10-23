c27f84311fa39465b519df2d22bb2977c7afb18f
Shaneen Vitug <shaneenvitug@gmail.com>
2019-10-23 19:53:20 +1100

    Fixed CSS style

--- shaneen_vitug/week_04/day3/main.rb
+++ shaneen_vitug/week_04/day3/main.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'HTTParty'
+require 'pry'
 
 get '/' do
   erb :home
@@ -13,6 +14,7 @@ get '/cover' do
     @url = "https://www.googleapis.com/books/v1/volumes?q=title:#{@title}"
     @book_info = HTTParty.get @url
     @cover_page = @book_info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+    @synopsis = @book_info["items"][0]["volumeInfo"]["description"]
   rescue
     redirect to('/')
   end
