345e3727f59d9f0b66f8e788b1ac3eab5bf9deb9
ncooney94 <nick.cooney@gmail.com>
2019-10-23 22:38:29 +1100

    working

---
+++ nicholas_cooney/week04/google_books/main.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
+
+
+
+get '/' do
+  erb :home
+end
+
+get '/book' do
+  @title = params[:title].downcase
+  @url = "https://www.googleapis.com/books/v1/volumes?q=title:#{@title}"
+  @book_info = HTTParty.get(@url)
+  @image = @book_info["items"].first["volumeInfo"]["imageLinks"]
+
+  @thumbnail = @image["thumbnail"]
+# binding.pry
+  erb :book
+end
