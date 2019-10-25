dafde87204c132ec1f90bb5bcf80423b4d8c6d91
Chris Kerr <chriskerr@me.com>
2019-10-26 08:46:45 +1100

    full basic functionality now exists

--- chris_kerr/week-4/fri/main.rb
+++ chris_kerr/week-4/fri/main.rb
@@ -9,7 +9,7 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Type < ActiveRecord::Base
-    belongs_to :heights
+    belongs_to :height
 end
 
 class Height < ActiveRecord::Base
@@ -26,6 +26,23 @@ get '/pry' do
     binding.pry
 end
 
+get '/add-cloud' do
+    @heights = Height.all
+    erb :new
+end
+
+post '/' do
+    cloud = Type.new
+    cloud.name = params[:name]
+    cloud.summary = params[:summary]
+    cloud.description = params[:description]
+    cloud.altitude_range = params[:altitude_range]
+    cloud.image = params[:image]
+    cloud.height_id = params[:height_id]
+    cloud.save
+    redirect to "/clouds/#{cloud.id}"
+end
+
 get '/:height' do
     @height = Height.find params[:height]
     erb :heights
@@ -34,6 +51,30 @@ end
 get '/clouds/:cloud' do
     @cloud = Type.find params[:cloud]
     erb :cloud
+end
+
+post '/clouds/:cloud' do
+    cloud = Type.find params[:cloud]
+    cloud.name = params[:name]
+    cloud.summary = params[:summary]
+    cloud.description = params[:description]
+    cloud.altitude_range = params[:altitude_range]
+    cloud.image = params[:image]
+    cloud.height_id = params[:height_id]
+    cloud.save
+    redirect to "/clouds/#{params[:cloud]}"
+end
+
+get '/clouds/:cloud/edit' do
+    @cloud = Type.find params[:cloud]
+    @heights = Height.all
+    erb :edit
+end
+
+get '/clouds/:cloud/delete' do
+    cloud = Type.find params[:cloud]
+    cloud.destroy
+    redirect to '/'
 end
 
 after do 
