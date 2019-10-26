e245edf15b64a2b86edca0b7d3c81838207eba0f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2019-10-26 19:21:20 +1100

    draft movies database

---
+++ jeffrey_quan/week04/movies_ar/directors.sql
@@ -0,0 +1,9 @@
+CREATE TABLE directors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  image TEXT
+);
+
+INSERT INTO directors (name, image) VALUES ('Christopher Nolan', '');
+INSERT INTO directors (name, image) VALUES ('Martin Scorses', '');
+INSERT INTO directors (name, image) VALUES ('Quentin Tarantino', '');
