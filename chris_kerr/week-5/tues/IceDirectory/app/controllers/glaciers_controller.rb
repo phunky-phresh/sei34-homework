391566f06d989b233fd14d1d58a8e08af36a50a3
Chris Kerr <chriskerr@me.com>
2019-10-29 22:20:05 +1100

    tues homework functionality complete

--- chris_kerr/week-5/tues/IceDirectory/app/controllers/glaciers_controller.rb
+++ chris_kerr/week-5/tues/IceDirectory/app/controllers/glaciers_controller.rb
@@ -26,5 +26,22 @@ class GlaciersController < ApplicationController
         @glacier = Glacier.find params[:id]
     end
 
+    def delete
+        glacier = Glacier.find params[:id]
+        glacier.destroy
+        redirect_to root_path
+    end
+
+    def submitEdit
+        glacier = Glacier.find params[:id]
+        glacier.name = params[:name]
+        glacier.length = params[:length]
+        glacier.volume = params[:volume]
+        glacier.image = params[:image]
+        glacier.country = params[:country]
+        glacier.save
+        redirect_to glacier_path(glacier.id)
+    end
+
 end
 
--- chris_kerr/week-5/tues/IceDirectory/config/routes.rb
+++ chris_kerr/week-5/tues/IceDirectory/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/new' => 'glaciers#new'
   post '/' => 'glaciers#addNew'
   get '/glaciers/:id' => 'glaciers#show', as: 'glacier'
+  post '/glaciers/:id' => 'glaciers#submitEdit'
   get '/glaciers/edit/:id' => 'glaciers#edit', as: 'edit_glacier'
+  delete '/glaciers/:id' => 'glaciers#delete', as: 'delete_glacier'
 end
