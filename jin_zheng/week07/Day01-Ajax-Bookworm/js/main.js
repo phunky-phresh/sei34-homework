74a5c06e55f4784e060f81138a283e29c38449e6
JinSeal <1183958@gmail.com>
2019-11-12 10:36:21 +1100

    refactored

--- jin_zheng/week07/Day01-Ajax-Bookworm/js/main.js
+++ jin_zheng/week07/Day01-Ajax-Bookworm/js/main.js
@@ -26,27 +26,6 @@ const updateCurrentBook = () => {
     document.getElementById('current-book-title').innerHTML = booklist[index]["volumeInfo"]["title"];
 }
 
-const addEaten = () => {
-    newDiv = document.createElement('div')
-    newDiv.className = "book"
-    newDiv.innerHTML = `<p class="book-title">${booklist[index]["volumeInfo"]["title"]}</p>`
-    newImg = document.createElement('img')
-    let attr = document.createAttribute("src");
-    attr.value = booklist[index]["volumeInfo"]["imageLinks"]["thumbnail"];
-    newImg.setAttributeNode(attr);
-    newImg.setAttribute("id", `thumb${index}`);
-    snakeImg.push(newImg)
-    newDiv.appendChild(newImg);
-    document.getElementById('eaten').appendChild(newDiv);
-}
-
-const createBook = (box) => {
-    const book = {
-    x : Math.floor(Math.random()*19) * box,
-    y : Math.floor(Math.random()*19) * box
-    };
-    return book
-}
 
 $(document).ready(() => {
     const canvas = document.getElementById("canvas");
@@ -56,6 +35,14 @@ $(document).ready(() => {
     headImg.src = "image/worm.jpg";
     let snakeImg = [headImg]
     let snake = [{ x : 10 * box, y : 10 * box }];
+
+    const createBook = () => {
+        const book = {
+        x : Math.floor(Math.random()*19) * box,
+        y : Math.floor(Math.random()*19) * box
+        };
+        return book
+    }
 
     let book = createBook(box)
 
@@ -90,6 +77,22 @@ $(document).ready(() => {
         return [snakeX, snakeY]
     }
 
+    const addEaten = () => {
+        newDiv = document.createElement('div')
+        newDiv.className = "book"
+        newDiv.innerHTML = `<p class="book-title">${booklist[index]["volumeInfo"]["title"]}</p>`
+        newImg = document.createElement('img')
+        let attr = document.createAttribute("src");
+        attr.value = booklist[index]["volumeInfo"]["imageLinks"]["thumbnail"];
+        newImg.setAttributeNode(attr);
+        newImg.setAttribute("id", `thumb${index}`);
+        snakeImg.push(newImg)
+        newDiv.appendChild(newImg);
+        document.getElementById('eaten').appendChild(newDiv);
+    }
+
+
+
     const draw =() => {
         ctx.fillStyle="black";
         ctx.fillRect(0,0,canvas.width,canvas.height);
@@ -99,7 +102,7 @@ $(document).ready(() => {
 
         for( let i = 0; i < snake.length ; i++){
             bookImg = snakeImg[i]
-            ctx.drawImage(bookImg, snake[i].x+1,snake[i].y+1,box-2,box-2);}
+            ctx.drawImage(bookImg, snake[i].x,snake[i].y,box-2,box-2);}
 
             let snakeX = snake[0].x;
             let snakeY = snake[0].y;
