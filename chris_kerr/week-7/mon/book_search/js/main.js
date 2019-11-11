65d55e4d4477b31fda573637d8bee3d524475543
Chris Kerr <chriskerr@me.com>
2019-11-12 09:56:05 +1100

    finished AJAX homework

--- chris_kerr/week-7/mon/book_search/js/main.js
+++ chris_kerr/week-7/mon/book_search/js/main.js
@@ -6,21 +6,33 @@
 
 // https://www.googleapis.com/books/v1/volumes?q=title:Ulysses
 
-const xhr = new XMLHttpRequest();
 
 
 let run = function() {
     let input = $('#search-box').val();
 
+    console.log('click')
+    const xhr = new XMLHttpRequest();
+
+    console.log(`https://www.googleapis.com/books/v1/volumes?q=title:${input}`)
+
     xhr.open("GET", `https://www.googleapis.com/books/v1/volumes?q=title:${input}`);
     xhr.send();
 
-    xhr.onreadystatechange = function() {
-    console.log(xhr.readyState)
-    if (xhr.readyState === 4) {
-        console.log('4')
+
+    xhr.onload = function() {
+        let data = JSON.parse(xhr.responseText)
+        let title = data.items[0].volumeInfo.title
+        let image = data.items[0].volumeInfo.imageLinks.smallThumbnail
+        let description = data.items[0].volumeInfo.description
+
+        $('div').append($(`<h3>${title}</h3>`))
+        $('div').append($(`<img src='${image}'></img>`))
+        $('div').append($(`<p>${description}</p>`))
     }
-}
+
+
+
 
     $('#search-box').val('')
 }
