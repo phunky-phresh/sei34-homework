37b4b264eca66640e5a14af62d00158a42cdd053
Chris Kerr <chriskerr@me.com>
2019-11-13 15:33:10 +1100

    image search done

--- chris_kerr/week-7/tues/ajax_api/js/main.js
+++ chris_kerr/week-7/tues/ajax_api/js/main.js
@@ -33,7 +33,7 @@ function updateIssLocation() {
 }
 
 
-let mymap = L.map('mapid').setView([20, 10], 2);
+let mymap = L.map('mapid').setView([25, 10], 2);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
