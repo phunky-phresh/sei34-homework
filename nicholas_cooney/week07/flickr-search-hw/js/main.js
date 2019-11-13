ee5563f541d1b60829e6ab8efa9a22053a5e7a15
ncooney94 <nick.cooney@gmail.com>
2019-11-13 23:25:58 +1100

    added a toggle display

--- nicholas_cooney/week07/flickr-search-hw/js/main.js
+++ nicholas_cooney/week07/flickr-search-hw/js/main.js
@@ -1,6 +1,7 @@
 const showImages = function(results) {
   //Nested Helper function
   console.log(results);
+
   const generateURL = function (p) {
     return [
       'http://farm',
@@ -18,43 +19,48 @@ const showImages = function(results) {
     const thumbnailURL = generateURL( photo );
     // const $img = $('<img>').attr('src', thumbnailURL)
     const $img = $('<img>', {src: thumbnailURL}); // same as line above
-    $img.appendTo('#images');
+    $img.appendTo('.images');
 
   })
 };
-  let firstPage = 1;
-  let page = 1;
-  let nextPage = 0;
-const searchFlickr = function (terms) {
-  const flickrURL = 'http://api.flickr.com/services/rest?jsoncallback=?';
-    p = firstPage;
-  $.getJSON(flickrURL, {
-    method: 'flickr.photos.search',
-    api_key: '2f5ac274ecfac5a455f38745704ad084', // This is not a secret key
-    text: terms,
-    format: 'json',
-    page: p
-  }).done(showImages);
-  let nextPage = firstPage += page;
-    p = nextPage;
+  let currentPage = 1
+  let totalPages = 1;
+  const searchFlickr = function (terms) {
+    const flickrURL = 'http://api.flickr.com/services/rest?jsoncallback=?';
+
+    $.getJSON(flickrURL, {
+      method: 'flickr.photos.search',
+      api_key: '2f5ac274ecfac5a455f38745704ad084', // This is not a secret key
+      text: terms,
+      format: 'json',
+      page: currentPage
+    }).done(showImages).done(function(result){
+            // console.log('retrieving page', targetPage, 'of', result.photos.pages)
+            totalPages = result.photos.pages;
+            console.log(totalPages)
+      // pages = results.photos.pages;
+      currentPage ++;
+      });
     // console.log(nextPage);
     // console.log(nextPage);
 
 
 };
-
 
 $(document).ready(function() {
 
   $('#search').on('submit', function (event) {
     event.preventDefault();
-    $('#images').empty(); //empties the div for new search items
-    // get search Terms
+
     const query = $('#query').val();
+    currentPage = 1;
+    $('.images').empty(); //empties the div for new search items
+    // get search Terms
     //fetch images from api
     searchFlickr( query );
   });
 
+let lastScrollBottom = 0;
   const limit = _.throttle(searchFlickr, 5000);
   $(window).on('scroll', function () {
     // console.log('scrolltop', $(window).scrollTop());
@@ -62,7 +68,7 @@ $(document).ready(function() {
     // console.log('document height', $(document).height());
 
     const scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-      if (scrollBottom < 650 ) {
+      if ( scrollBottom < 650 && totalPages > 1) {
         // get query
         const query = $('#query').val();
         limit(query);
@@ -70,6 +76,12 @@ $(document).ready(function() {
 
       }
   });
+
+  $('#four').on('click', function () {
+
+    $('.images').toggleClass('images2');
+  });
+
 
 });
 
