ecc88341439f5556e12a5a94df02eb3790f86aba
Amanda Chau <amandachau36@gmail.com>
2019-11-15 10:35:43 +1100

    grains solution - jeffery

--- warmup/week07/day04_grains/js/main.js
+++ warmup/week07/day04_grains/js/main.js
@@ -28,3 +28,30 @@ console.log( 'Square 3:', grains.grainsOnSquare(3));
 grains.grainsOnAllSquares();
 
 console.log('Total grains on board:', grains.totalGrainsOnBoard() );
+
+
+/// Jeffery's solution with underscore.js
+const grains = {
+  maxNoOfSquares: 64,
+  calcGrainsOnASquare: function(n) {
+    noOfGrains = 2 ** (n-1);
+    return noOfGrains;
+  },
+  noOfGrainsOnEachSquare: function(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log(`Square ${ i }: ${ 2 ** (i - 1) }`)
+    }
+  },
+  totalGrains: function() {
+    const array = [...Array(this.maxNoOfSquares).keys()].map(x => ++x)
+    const grainsOnBoard = array.map(x => 2**(x-1));
+    const sum = _(grainsOnBoard).reduce(function(runningTotal, n) {
+      return runningTotal + n
+    });
+    return sum;
+  }
+}
+console.log( grains.calcGrainsOnASquare(6) );
+grains.noOfGrainsOnEachSquare(64);
+console.log( grains.totalGrains() );
+////
