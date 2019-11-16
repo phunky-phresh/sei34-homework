f73525db7d6825bdfb45c6409c9b4a68f65f0521
JinSeal <1183958@gmail.com>
2019-11-16 23:20:56 +1100

    Day04 && Day05

--- jin_zheng/week07/Day03-enhanced-flickr/js/search.js
+++ jin_zheng/week07/Day03-enhanced-flickr/js/search.js
@@ -40,26 +40,28 @@ $(document).ready(() => {
         searchFlickr($("#query").val());
     });
 
-    $(window).scroll(_.throttle(() => {
+    const debouncedSearchFlickr = _.debounce(searchFlickr, 1000, { trailing: false });
+
+    $(window).scroll(() => {
         let scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-        scrollBottom < 200 &&  searchFlickr($("#query").val())}, 600));
+        scrollBottom < 200 && debouncedSearchFlickr($("#query").val())});
 
-        let map = L.map('map').setView([0, 0], 10);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            maxZoom: 18,
-            id: 'mapbox.light',
-            accessToken: 'pk.eyJ1Ijoiamluc2VhbCIsImEiOiJjazJ2c2tybDMwOGhnM2JvMXltd2NqYWJlIn0.coec8eyI6d1tTfDQvcffTg'
-        }).addTo(map);
+    let map = L.map('map').setView([0, 0], 10);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'mapbox.light',
+        accessToken: 'pk.eyJ1Ijoiamluc2VhbCIsImEiOiJjazJ2c2tybDMwOGhnM2JvMXltd2NqYWJlIn0.coec8eyI6d1tTfDQvcffTg'
+    }).addTo(map);
 
-        L.TileLayer.Photo = L.TileLayer.extend({
-            getTileUrl: function(coords) {
-                return _(photoURL).sample();
-            },
-        });
-
-        L.tileLayer.photo = function() {
-            return new L.TileLayer.Photo();
-        }
-
-        L.tileLayer.photo().addTo(map);
+    L.TileLayer.Photo = L.TileLayer.extend({
+        getTileUrl: function(coords) {
+            return _(photoURL).sample();
+        },
     });
+
+    L.tileLayer.photo = function() {
+        return new L.TileLayer.Photo();
+    }
+
+    L.tileLayer.photo().addTo(map);
+});
